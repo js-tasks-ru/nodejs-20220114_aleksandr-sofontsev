11fb4987565d2f014a966d3cf6153b68b67fecc0
ithillelasofontsev <svyazist2@gmail.com>
2022-02-02 02:33:00 +0300

    - task 3 (part 2)

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,22 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.str = '';
+    this.arrStr = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.str += chunk;
+    this.arrStr = this.str.split(os.EOL);
+    this.str = this.arrStr.pop();
+    callback(null, this.arrStr.shift());
+    for (const item of this.arrStr) {
+      this.push(item);
+    }
   }
 
   _flush(callback) {
+    callback(null, this.str);
   }
 }
 
