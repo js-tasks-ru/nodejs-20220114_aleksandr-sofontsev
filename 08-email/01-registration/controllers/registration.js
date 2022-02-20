80af23f5214bc01a66e9ce5fb8f196bce83208fb
ithillelasofontsev <svyazist2@gmail.com>
2022-02-20 18:04:58 +0300

    - task 8 (part 1)

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,43 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const verificationToken = uuid();
+  const user = await User.create({
+    email: ctx.request.body.email,
+    displayName: ctx.request.body.displayName,
+    verificationToken,
+  });
 
+  await user.setPassword(ctx.request.body.password);
+  await user.save();
+
+  await sendMail({
+    to: user.email,
+    locals: {token: verificationToken},
+    subject: 'Подтвердите почту',
+    template: 'confirmation',
+  });
+
+  ctx.status = 200;
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const user = await User.findOne({
+    verificationToken: ctx.request.body.verificationToken,
+  });
 
+  if (!user) {
+    ctx.status = 400;
+    ctx.body = {error: 'Ссылка недействительна или устарела'};
+    return;
+  }
+
+  user.verificationToken = undefined;
+  await user.save();
+
+  ctx.status = 200;
+  ctx.body = {token: uuid()};
 };
